5cf9343f72f71c0951c7d4ec950ce37e08f66e35
Kangbeen <ufon2011@gmail.com>
2025-04-29 13:40:43 +0900

    250426~250429 오전까지 front-end 작업한 것 추가

---
+++ Front-end/src/pages/MainPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './MainPage.css'; // 스타일 파일 따로
+import { useNavigate } from 'react-router-dom';
+
+export default function MainPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="main-page">
+      {/* 헤더 */}
+      <header className="header">
+        <div
+          className="logo"
+          style={{ cursor: 'pointer' }}
+          onClick={() => navigate('/')}
+        >
+          🏡 살아보고서
+        </div>
+        <div className="auth-buttons">
+          <button onClick={() => navigate('/login')}>로그인</button>
+          <button onClick={() => navigate('/signup')}>회원가입</button>
+        </div>
+      </header>
+
+      {/* 본문 */}
+      <div className="main-content">
+        {/* 사이드 메뉴 */}
+        <aside className="sidebar">
+          <ul>
+            <li onClick={() => navigate('/survey')}>설문</li>
+            <li onClick={() => navigate('/chatbot')}>챗봇</li>
+          </ul>
+        </aside>
+
+        {/* 본 내용 */}
+        <section className="content">
+          메인 페이지 입니다.
+        </section>
+      </div>
+    </div>
+  );
+}
